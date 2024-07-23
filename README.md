84a130138f9b02d113ef2100c045e7c3baf832ef
afzal-1002 <mrafzal1002@gmail.com>
2024-07-24 00:12:03 +0200

    message

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# git-new
+ <h1>Read me </h1>
+ <h2>Test File</h2>
+ <br>
+ Muhammad Afzal
+
+
--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const PORT = 3000;
 
 app.get("/user", (req, res) => {
     console.log('This is a new user app');
-    res.send("User is requesting '/ user url");
+    res.send("<h1> User is requesting '/ user url<h1>");
 })
 
 app.listen(PORT, ()=>{
-    console.log(`App is listening at port ${PORT}`);
+    console.log(`port is listening at https://localhost:${PORT}`);
+    console.log(`Check user url  https://localhost:${PORT}/user`);
 })
