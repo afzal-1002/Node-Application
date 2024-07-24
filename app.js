cbf93eefd81a9771154d2f5c23c993eba16b0cc8
afzal-1002 <mrafzal1002@gmail.com>
2024-07-24 18:56:32 +0200

    app.js updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.get("/user", (req, res) => {
     console.log('This is a new user app');
-    res.send("User is requesting '/ user url");
+    res.send("<h1>User is requesting '/ user url</h1>");
 })
 
 app.listen(PORT, ()=>{
